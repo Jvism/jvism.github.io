29ee1cb0d5f264832b362ede62bf3327f958a55d
Jvism <jvi.smo@gmail.com>
2021-02-21 02:02:40 -0300

    add_animation_open_menu

--- resources/scripts/nav.js
+++ resources/scripts/nav.js
@@ -1,12 +1,24 @@
 const menuBtn = document.querySelector(".navbar-toggle");
+const menuDiv = document.querySelector(".nav-menu");
+const menuOverlay = document.querySelector(".overlay-menu");
+const menuBorders = document.querySelector(".borders");
+
+
 let menuOpen = false;
 
 menuBtn.addEventListener("click", () => {
     if(!menuOpen) {
         menuBtn.classList.add('open');
+        menuDiv.classList.replace('close-menu','open-menu');
+        menuOverlay.classList.replace('close-menu-overlay','open-menu-overlay');
+        menuBorders.classList.add('open-menu-borders');
         menuOpen = true;
+
     } else{
         menuBtn.classList.remove('open');
+        menuDiv.classList.replace('open-menu','close-menu');
+        menuOverlay.classList.replace('open-menu-overlay','close-menu-overlay');
+        menuBorders.classList.remove('open-menu-borders');
         menuOpen = false;
     }
 });
