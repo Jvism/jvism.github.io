3b053d838d838cfa14e9d313a818e77f9f52f333
Jvism <jvi.smo@gmail.com>
2021-02-21 14:57:59 -0300

    scroll_touchScreen

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -3,18 +3,22 @@ window.addEventListener("load", () => {
     const navMenu = document.querySelector(".nav-menu");
 
     let scroll_value = 0;
-    let canScroll = true;
+    var canScroll = true;
 
-    window.addEventListener("mousewheel", (e) => {
+    // variables deteccion de arrastre
 
-        if(canScroll){
+    let startTouch = 0;
+    let endTouch = 0;
 
-            
+    function scroll(boolScroll,value){
+
+        if(boolScroll){
+
             canScroll = false;
 
             if(navMenu.classList.contains("close-menu")){
                 
-                if (e.deltaY > 0) {
+                if (value > 0) {
                     if (scroll_value < 30) {
                         scroll_value += 1;
                     }
@@ -23,7 +27,7 @@ window.addEventListener("load", () => {
                         scroll_value -= 1;
                     }
                 }
-
+                
                 animation(scroll_value);
     
             };
@@ -32,6 +36,29 @@ window.addEventListener("load", () => {
         setTimeout(function(){
             canScroll = true;
         }, 1000);
+    }
+
+    window.addEventListener("touchstart", (e) => {
+
+        startTouch = e.changedTouches[0].screenY;
+
+        window.addEventListener("touchend", (a) => {
+            endTouch = a.changedTouches[0].screenY;
+        });
+
+        let direction_touchMove = startTouch - endTouch;
+
+        if(direction_touchMove > 150 || direction_touchMove < -150){
+
+            scroll(canScroll,direction_touchMove);
+        }
+
+    });
+
+
+    window.addEventListener("mousewheel", (e) => {
+
+        scroll(canScroll,e.deltaY);
 
     });
 
